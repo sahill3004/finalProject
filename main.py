724b5c30a739b83cbbc558a367d25764584b5902
sahill3004 <sahilnaik3004@gmail.com>
2023-06-03 17:36:03 +0530

    Implemented basic view for all pages

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ from flask import Flask, render_template, redirect, url_for
 from Scrappers import flipkartScrapping as scrap
 from Scrappers import amazonScrapping as amazonScrap
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    productDetailsFlipkart = scrap.flipkartScrapper()
-    productDetailsAmazon = amazonScrap.amazonScrapper()
+def index():
     return render_template("index.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/compare")
+def compare():
+    return render_template("compare.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
